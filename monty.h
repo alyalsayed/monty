cbe0aa70a2a64952ebca0924285e0b7fc8c6c682
alyalsayed <alyalsayed56@yahoo.com>
2023-05-25 15:13:48 +0300

    done : update monty.h

--- monty.h
+++ monty.h
@@ -56,29 +56,30 @@ char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
 
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
+/* Stack operations functions */
+void my_push(stack_t **head, unsigned int number);
+void my_pall(stack_t **head, unsigned int number);
+void my_pint(stack_t **head, unsigned int number);
 int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
+void my_pop(stack_t **head, unsigned int counter);
+void my_swap(stack_t **head, unsigned int counter);
+void my_nop(stack_t **head, unsigned int counter);
 
-/*Functions math*/
-void f_add(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
+/*Stack math functions */
+void my_add(stack_t **head, unsigned int counter);
+void my_sub(stack_t **head, unsigned int counter);
+void my_div(stack_t **head, unsigned int counter);
+void my_mul(stack_t **head, unsigned int counter);
+void my_mod(stack_t **head, unsigned int counter);
 
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+void my_pchar(stack_t **head, unsigned int counter);
+void my_pstr(stack_t **head, unsigned int counter);
+void my_rotl(stack_t **head, unsigned int counter);
+void my_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void add_node(stack_t **head, int n);
+void add_queue(stack_t **head, int n);
+void my_queue(stack_t **head, unsigned int counter);
+void my_stack(stack_t **head, unsigned int counter);
 
 #endif
