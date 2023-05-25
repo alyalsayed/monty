9d07b642c7189a29d6e92af7cf282cb5bbfd840a
alyalsayed <alyalsayed56@yahoo.com>
2023-05-25 15:25:49 +0300

    done : upload nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *my_nop - nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void my_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
