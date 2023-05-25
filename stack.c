26a019b6ecca037750f269aae9275544070331af
alyalsayed <alyalsayed56@yahoo.com>
2023-05-25 15:30:12 +0300

    done : uploading new c files

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * my_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void my_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
